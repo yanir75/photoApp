9c339a559b2ade8068b6a8c94c8ae29099983147
yanir <yanir@macs-MacBook-Pro.local>
2024-05-03 06:11:17 +0300

    mixing with auth0

---
+++ upload_service/platform/router/router.go
@@ -0,0 +1,42 @@
+package router
+
+import (
+	"encoding/gob"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+
+	"update_service/platform/authenticator"
+	"update_service/platform/middleware"
+	// "update_service/platform/uploader"
+	"update_service/web/app/callback"
+	"update_service/web/app/home"
+	"update_service/web/app/login"
+	"update_service/web/app/logout"
+	"update_service/web/app/user"
+)
+
+// New registers the routes and returns the router.
+func New(auth *authenticator.Authenticator) *gin.Engine {
+	router := gin.Default()
+
+	// To store custom types in our cookies,
+	// we must first register them using gob.Register
+	gob.Register(map[string]interface{}{})
+
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("auth-session", store))
+
+	router.Static("/public", "web/static")
+	router.LoadHTMLGlob("web/template/*")
+
+	router.GET("/", home.Handler)
+	router.GET("/login", login.Handler(auth))
+	router.GET("/callback", callback.Handler(auth))
+	router.GET("/user", middleware.IsAuthenticated, user.Handler)
+	router.GET("/logout", logout.Handler)
+	// router.POST("/upload",uploader.uploadFile)
+
+	return router
+}
