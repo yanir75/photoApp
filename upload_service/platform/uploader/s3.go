bf754893cb689d1fb624efd73d4ac7ee98d27f43
Yanir <yanircohen75@gmail.com>
2024-05-04 19:16:20 +0300

    added multiple files upload

--- upload_service/platform/uploader/s3.go
+++ upload_service/platform/uploader/s3.go
@@ -65,7 +65,7 @@ func uploadFileToS3(fileName string, fileContent []byte, description string) (st
 	})
 	if err != nil {
 		fmt.Println("Error uploading file:", err)
-		return "", err
+		return "Not Uploaded successfully", err
 	}
 
 	fmt.Println("File uploaded successfully!!!")
@@ -94,39 +94,45 @@ func uploadFile(ctx *gin.Context) {
 	// FormFile returns the first file for the given key `myFile`
 	// it also returns the FileHeader so we can get the Filename,
 	// the Header and the size of the file
-	file, handler, err := r.FormFile("myFile")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		fmt.Fprintf(w, "Error Retrieving the File")
-		return
-	}
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	fhs := r.MultipartForm.File["myFiles"]
+	msg := ""
+	for index, handler := range fhs {
+		file, err := handler.Open()
+		if err != nil {
+			fmt.Println("Error Retrieving the File")
+			fmt.Println(err)
+			fmt.Fprintf(w, "Error Retrieving the File")
+			return
+		}
+		defer file.Close()
+		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+		fmt.Printf("File Size: %+v\n", handler.Size)
+		fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-	// Create a temporary file within our temp-images directory that follows
-	// a particular naming pattern
+		// Create a temporary file within our temp-images directory that follows
+		// a particular naming pattern
 
-	// read all of the contents of our uploaded file into a
-	// byte array
-	fileBytes, err := io.ReadAll(file)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, err.Error())
-		errors = true
-	}
+		// read all of the contents of our uploaded file into a
+		// byte array
+		fileBytes, err := io.ReadAll(file)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, err.Error())
+			errors = true
+		}
 
-	msg, err := uploadFileToS3(country+"/"+fileName, fileBytes, description)
-	if err != nil {
-		fmt.Fprint(w, err.Error())
-		errors = true
+		message, err := uploadFileToS3(fmt.Sprintf("%s/%s-%d",country,fileName,index), fileBytes, description)
+		if err != nil {
+			fmt.Fprint(w, err.Error())
+			msg += fmt.Sprintf("File number %d: was not %s \\n", index, message)
+		} else {
+			msg += fmt.Sprintf("File number %d: was %s \\n", index, message)
+		}
 	}
 	// write this byte array to our temporary file
 	// return that we have successfully uploaded our file!
 	if !errors {
-		ctx.Redirect(http.StatusSeeOther,"/upload?msg="+msg)
+		ctx.Redirect(http.StatusSeeOther, "/upload?msg="+msg)
 	}
 }
 
