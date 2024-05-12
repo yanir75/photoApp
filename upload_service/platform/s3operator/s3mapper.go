4c63d1feeb461a3849560df2da089e843f46e9ca
Yanir <yanircohen75@gmail.com>
2024-05-13 00:06:03 +0300

    added s3 signed url

--- upload_service/platform/s3operator/s3mapper.go
+++ upload_service/platform/s3operator/s3mapper.go
@@ -4,6 +4,8 @@ import (
 	
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"time"
+
 )
 
 func GetS3Folders(){
@@ -18,10 +20,38 @@ func GetS3Folders(){
 
 	bucket := conf.BucketName
 
-	li,_ :=svc.ListObjectsV2(&s3.ListObjectsV2Input{
+	li,err :=svc.ListObjectsV2(&s3.ListObjectsV2Input{
 		Bucket: aws.String(bucket),
 		Delimiter: aws.String("/"),
 		Prefix: aws.String(""),
 	})
-	fmt.Println(li.CommonPrefixes)
+	if err != nil {
+		fmt.Println("Error listing folders:", err)
+	}
+	fmt.Println(li.CommonPrefixes[0].String())
+}
+
+func UrlSigner(objectName string , timeInMinutesToSign ... int) (string){
+	sess,err := getSession()
+	timeInMinutes := 15
+	if len(timeInMinutesToSign) > 0 {
+		timeInMinutes = timeInMinutesToSign[0]
+	}
+	if err != nil {
+		fmt.Println("Error creating session:", err)
+		// return "", err
+	}
+	svc := s3.New(sess)
+
+	bucket := conf.BucketName
+    req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+        Bucket: aws.String(bucket),
+        Key:    aws.String(objectName),
+    })
+    urlStr, err := req.Presign(time.Duration(timeInMinutes) * time.Minute)
+
+    if err != nil {
+        fmt.Println("Failed to sign request", err)
+    }
+    return urlStr
 }
